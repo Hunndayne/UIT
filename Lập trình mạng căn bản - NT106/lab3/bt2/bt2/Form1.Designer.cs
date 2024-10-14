cdb05862b73e487e1f1d4fbbd8823deb9c87cf36
Hùng Trần Thanh <cuteogaming@gmail.com>
2024-10-15 01:34:32 +0700

    FULL SQL HOMEWORK

---
+++ Lập trình mạng căn bản - NT106/lab3/bt2/bt2/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace bt2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            StartListen = new Button();
+            listView = new ListView();
+            SuspendLayout();
+            // 
+            // StartListen
+            // 
+            StartListen.Location = new Point(410, 40);
+            StartListen.Name = "StartListen";
+            StartListen.Size = new Size(75, 23);
+            StartListen.TabIndex = 1;
+            StartListen.Text = "Listen";
+            StartListen.UseVisualStyleBackColor = true;
+            StartListen.Click += StartListen_Click;
+            // 
+            // listView
+            // 
+            listView.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            listView.Location = new Point(22, 66);
+            listView.Name = "listView";
+            listView.Size = new Size(463, 363);
+            listView.TabIndex = 2;
+            listView.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(527, 450);
+            Controls.Add(listView);
+            Controls.Add(StartListen);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button StartListen;
+        private ListView listView;
+    }
+}
---
+++ Lập trình mạng căn bản - NT106/lab3/bt2/bt2/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace bt2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartListen_Click(object sender, EventArgs e)
+        {
+            Thread serverThread = new Thread(new ThreadStart(StartUnsafeThread));
+            serverThread.IsBackground = true; // Make it a background thread so it stops when the application closes
+            serverThread.Start();
+        }
+
+        void StartUnsafeThread()
+        {
+            try
+            {
+                int bytesReceived = 0;
+                byte[] recv = new byte[1024]; // Use a larger buffer size to improve efficiency
+
+                // Create the listening socket
+                Socket listenerSocket = new Socket(
+                    AddressFamily.InterNetwork,
+                    SocketType.Stream,
+                    ProtocolType.Tcp);
+
+                IPEndPoint ipepServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+                listenerSocket.Bind(ipepServer);
+                listenerSocket.Listen(10);
+
+                AppendTextToListView("Server started, waiting for connections...");
+
+                // Accept client connection
+                Socket clientSocket = listenerSocket.Accept();
+                AppendTextToListView("New client connected.");
+
+                // Receive data from the client
+                while (clientSocket.Connected)
+                {
+                    string text = "";
+                    do
+                    {
+                        bytesReceived = clientSocket.Receive(recv);
+                        text += Encoding.UTF8.GetString(recv, 0, bytesReceived);
+                    } while (text.Length == 0 || text[text.Length - 1] != '\n');
+
+                    AppendTextToListView(text.Trim());
+                }
+
+                // Clean up sockets
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+                listenerSocket.Close();
+            }
+            catch (Exception ex)
+            {
+                AppendTextToListView($"Error: {ex.Message}");
+            }
+        }
+
+        private void AppendTextToListView(string text)
+        {
+            if (listView.InvokeRequired)
+            {
+                listView.Invoke((MethodInvoker)delegate
+                {
+                    listView.Items.Add(new ListViewItem(text));
+                });
+            }
+            else
+            {
+                listView.Items.Add(new ListViewItem(text));
+            }
+        }
+    }
+}
