15a760f7c9500f8edb7516e989b530e55a3eacb6
Hùng Trần Thanh <cuteogaming@gmail.com>
2024-11-12 10:12:35 +0700

    Create 18 BST - CountLeaves.cpp

---
+++ Cấu trúc dữ liệu và giải thuật - IT003/18 BST - CountLeaves.cpp
@@ -0,0 +1,60 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+###End banned keyword*/
+
+#include <iostream>
+using namespace std;
+
+struct TNODE {
+    int key;
+    TNODE* pLeft;
+    TNODE* pRight;
+};
+typedef TNODE* TREE;
+
+
+void InsertNode(TREE& T, int x) {
+    if (T == nullptr) {
+        T = new TNODE;
+        T->key = x;
+        T->pLeft = T->pRight = nullptr;
+    }
+    else {
+        if (x < T->key) {
+            InsertNode(T->pLeft, x);
+        }
+        else if (x > T->key) {
+            InsertNode(T->pRight, x);
+        }
+
+    }
+}
+
+void CreateTree(TREE& T) {
+    T = nullptr;
+    int value;
+    bool isEmpty = true;
+
+
+    while (cin >> value && value != -1) {
+        isEmpty = false;
+        InsertNode(T, value);
+    }
+}
+
+int CountLeaf(TREE T) {
+    if (T == NULL)
+        return 0;
+    if (T->pLeft == nullptr && T->pRight == nullptr) {
+        return 1;
+    }
+    return CountLeaf(T->pLeft) + CountLeaf(T->pRight);
+}
+
+int main() {
+    TREE T; //hay: TNODE* T;
+    T = NULL; // Khoi tao cay T rong, or: CreateEmptyTree(T)
+    CreateTree(T);
+
+    cout << CountLeaf(T);
+    return 0;
+}
